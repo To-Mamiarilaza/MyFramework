5a9abd99fc3c7dbf1ce2f91779e3a36dcb769fff
To-Mamiarilaza <mamiarilaza.to@gmail.com>
2023-03-14 11:37:08 +0300

    Finition sprint 3

---
+++ src/java/etu1964/model/Emp.java
@@ -0,0 +1,48 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package etu1964.model;
+
+import etu1964.framework.annotations.Url;
+import java.util.List;
+
+/**
+ *
+ * @author to
+ */
+public class Emp {
+/// Attributs
+
+    String nom;
+
+/// Encapsulation
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String nom) throws Exception {
+        if (nom.equals("") || nom == null) {
+            throw new Exception("Le nom ne doit pas etre null");
+        }
+        this.nom = nom;
+    }
+
+/// Constructeur
+    public Emp(String nom) throws Exception {
+        setNom(nom);
+    }
+
+/// Fonctions du classe
+    
+    @Url(path="getAllEmployee")
+    public List getAll() {
+        System.out.println("Listes de tous les employées");
+        return null;
+    }
+
+    @Url(path="insertEmp")
+    public void insertionEmp() {
+        System.out.println("Insertion nouveau Emp");
+    }
+}
